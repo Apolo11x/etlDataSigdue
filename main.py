5bad2e7aafbc02d477e5df71f5c1406657fcda0a
Apolo11x <javier.arley.sanchez.ledesma@gmail.com>
2025-06-24 11:39:49 -0500

    Ajustando programa principal main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from etl.etl_anexo_6a import procesar_anexo_6a
 # Ejemplo de uso de la función etl_anexo_6a
 # Supongamos que la función recibe una ruta de archivo de entrada y una de salida
 
+
 input_file = 'ANEXO_6A_CORTE_01062025.csv'
 output_file = 'processed_ANEXO_6A_CORTE_01062025.csv'
 
